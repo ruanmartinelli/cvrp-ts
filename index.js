ece65a5a88355aa222766c450f698b546991e23b
Ruan Martinelli <martinelliruan@gmail.com>
2016-10-20 02:15:02 -0200

    fix route cost calculation

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const distance = require('euclidean-distance');
 const chalk = require('chalk');
 const debug = (text) => console.log(chalk.green.bold("[DEBUG] ") + chalk.blue.bold(text))
 const success = (text) => console.log(chalk.green.bold("[LOG] "+ text + " ✔️"))
+const equal = require('deep-equal')
+
 parser.tsplib(process.argv.slice(2), (res) => {
     // console.log(res[0]);
     tabu(res[0]);
@@ -14,14 +16,14 @@ let CAPACITY = 0;
 
 function tabu(graph){
     const V = graph.vertices;
-    const max_iterations = 1000;
+    const max_iterations = 100;
 
     CAPACITY = graph.capacity;
 
     let cost_matrix = find_cost_matrix(V, graph.dimension)
     success("Cost matrix OK");
 
-    let initial_solution = find_initial_solution(V);
+    let initial_solution = find_initial_solution(V, {id: 1, demand:0});
     success("Initial Solution OK (" + initial_solution.iterations + " iteration(s))")
 
     initial_solution.cost = find_solution_cost(initial_solution, cost_matrix)
@@ -29,11 +31,13 @@ function tabu(graph){
 
     //test
     let count = 0;
-    let current = _.clone(initial_solution);
-    let best = _.clone(current);
+    let current = _.cloneDeep(initial_solution);
+    let best = _.cloneDeep(current);
+    let tabu_list = [];
+    let tabu_list_size = 1;
 
-    while(count < max_iterations){
-        let candidate = _.clone(current);
+    while(count < 22000){
+        let candidate = _.cloneDeep(current);
 
         // rsh = route swap helper
         let rsh = random_routes(candidate.routes);
@@ -47,16 +51,45 @@ function tabu(graph){
         // removes node1 from route1
         candidate.routes[route1_index].splice(rsh.node1_index, 1);
 
-        // TODO: evaluate costs and add tabu methods
-        // TODO: make sure solution is not better than optimal
+        // TODO: fix this, tabu list is not working properly yet
+        if(!is_tabu(tabu_list, candidate.routes)){
+            const cost_is_better = find_solution_cost(candidate, cost_matrix) < find_solution_cost(current, cost_matrix);
+            if(cost_is_better){
+                current = _.cloneDeep(candidate);
 
+                const cost_is_best = find_solution_cost(candidate, cost_matrix) < find_solution_cost(best, cost_matrix);
+
+                if(cost_is_best){
+                    console.log("Found new best", find_solution_cost(candidate, cost_matrix));
+                    best = _.cloneDeep(candidate);
+                }
+            }
+            add_to_tabu(tabu_list, current.routes, tabu_list_size);
+        }else{
+            console.log("Ops, tabu found");
+        }
         count++;
     }
+
+    console.log(find_solution_cost(best, cost_matrix));
     success("Tabu search finished on "+count+ " iterations.");
 }
 
-function delete_node(node, route){
+function add_to_tabu(tabu_list, routes, tabu_list_size){
+    let r = _.cloneDeep(routes)
+    tabu_list.push(r);
+    if(tabu_list.length > tabu_list_size){
+        tabu_list.shift();
+    }
+}
 
+function is_tabu(tabu_list, routes){
+    _.forEach(tabu_list, forbidden_routes => {
+        if(equal(routes, forbidden_routes)){
+            return true;
+        }
+    })
+    return false;
 }
 
 function add_node_before(new_node, reference_node, route){
@@ -75,15 +108,13 @@ function random_routes(routes){
     let node1_index = 0;
     while(1){
         while(1){
-
             route1 = _.sample(routes);
             route2 = _.sample(routes);
-            if(route1.length > 3) break;
+            if(route1.length > 3 && (route1 != route2)) break;
         }
 
-        node1 = _.sample(route1);
+        node1 = _.sample(_.slice(route1, 1, route1.length - 1));
         node1_index = _.findIndex(route1, (n) => _.isEqual(n, node1));
-
         node2 = find_closest(node1, route2);
 
         if(find_route_demand(route2) + node1.demand < CAPACITY) break;
@@ -112,29 +143,36 @@ function node_distance(node1, node2){
 }
 
 function find_solution_cost(solution, cost_matrix){
-    const routes = solution.routes;
-    let costs = [];
+    let sum = 0;
 
-    _.forEach(routes, (route) => {
-        let route_cost = 0;
-        for(var i = 0; i < route.length -1; i++){
-            route_cost += cost_matrix[route[i].id -1][route[i+1].id -1]
+    let routes = [];
+
+    routes = _.flattenDeep(solution.routes);
+    _.map(routes, (node, index) => {
+        let current_node = routes[index]
+
+        if(index < routes.length -1){
+            let next_node = routes[index + 1]
+            let cost = cost_matrix[current_node.id - 1][next_node.id - 1];
+            sum += cost;
         }
-        costs.push(_.toInteger(route_cost.toFixed(2)))
     })
-
-    const total_cost = _.reduce(costs, (sum,v) => sum += v , 0)
-
-    return total_cost;
+    return sum;
 }
 
-function find_initial_solution(vertices){
+function find_initial_solution(vertices, depot_node){
     const chunk_size = vertices.length / TRUCK_AMOUNT;
     let routes = [];
     let iterations = 0;
     while(true){
         iterations ++;
         routes = _.chunk(_.shuffle(vertices), chunk_size);
+
+        // adds depot node to end of every route
+        _.map(routes, route => {
+            route.push(depot_node);
+        });
+
         let route_demands = _.map(routes, calculate_demand_from_route);
         if(_.every(route_demands, (riv) => riv < 100)) break;
     }
@@ -150,8 +188,6 @@ function calculate_demand_from_route(route){
 }
 
 function find_cost_matrix(vertices, dimension){
-    debug("Dimension: " + dimension)
-
     let cost_matrix = [];
 
     for(var i = 0; i < vertices.length; i++){
